2f2665afe878070b83cf4f4563aeb2fcff14e9d4
song <ottstormtrooper@gmail.com>
2024-10-30 22:57:54 -0400

    443

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 443;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/hello') {
