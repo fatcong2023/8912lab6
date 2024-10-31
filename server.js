0dfa58b6b98bf61ac2c0bfd16425ec62bbe51b4b
song <ottstormtrooper@gmail.com>
2024-10-30 22:20:23 -0400

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/hello') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('hello world');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
